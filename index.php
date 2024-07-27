5373a12886eb8999734758aeb4c315a6c87c84b0
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2024-07-27 12:31:38 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,313 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Title -->
+    <title>WaTer-SerVe.com</title>
+    <!-- styling.css link -->
+    <script src="https://kit.fontawesome.com/9b1965001a.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="Images\waterlogo.jpg">
+    <link rel="stylesheet" href="landing.css" />
+
+    <!-- Favicon -->
+    
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<style>
+ /* footer */
+
+footer{
+	background-color:rgb(0, 139, 185);
+	color: #150202;
+	font-weight: bold;
+	text-decoration-line: none;
+	text-align: center;
+}
+
+.footer-link-item1{
+     color: #d3d3d3;
+     font-weight: bold;
+     text-decoration-line: none;
+}
+.footer-link-item:hover{
+     color: rgb(19, 19, 19);
+     font-weight: normal;
+}
+.icon-link{
+     color: rgb(255, 255, 255);
+     font-size: 40px;
+     font-weight: bold;
+}
+.icon-link:hover{
+     color: rgb(19, 19, 19);
+     font-weight: normal;
+}
+.custom-shadow {
+            box-shadow: 0 4px 20px rgba(226, 9, 9, 0.911); /* Adjust the color and opacity as needed */
+        }
+
+</style>
+
+
+
+<!-- HTML content -->
+
+    <!-- Navigation bar code starts here -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="landing.html">
+            <img src="Images\waterlogo.jpg" height="70" alt="Your choise logo">
+        </a>
+            <label class="logo" >AquaTrack <small>.com </small></label>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item footer-link-item">
+                    <a class="nav-link " style="color: rgb(0, 149, 199);"href="landing.php">Home</a>
+                </li>
+                
+                <li class="nav-item active footer-link-item">
+                    <a class="nav-link text-dark" href="register.html">Log In</a>
+                </li>
+                
+    <li class="nav-item active footer-link-item">
+        <a class="nav-link text-dark" href="dashboard.php">Portal</a>
+    </li>
+
+                <li class="nav-item footer-link-item">
+                    <a class="nav-link text-dark" href="contact.html">Contact Us</a>
+                </li>
+                <li class="nav-item footer-link-item">
+                    <a class="nav-link text-dark" href="aboutus.html">About Us</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- Navigation bar code ends here -->
+
+    <!-- backgroundimage for welcome message code starts here -->
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col-12 position-relative" style="height: 100vh;">
+            <img src="Images/pexels-lastly-590178.jpg" alt="Deepsea" class="img-fluid w-100 h-100" style="object-fit: cover;">
+            <div class="overlay position-absolute w-100 h-100 d-flex justify-content-center align-items-center" style="top: 0; left: 0;">
+              <div class="centered-text text-center text-white">
+                    <img src="Images\waterlogo.jpg" height="200" alt="Your choise logo">
+                <p>Streamlining Water Solutions</p>
+                <p>Connect, Communicate, and Resolve All Your Piping Issues Effortlessly.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- backgroundimage for welcome message code ends here -->
+
+    
+    <div class="row shadow py-3 my-3 justify-content-center" style="background-color: white; box-shadow: 0 4px 20px rgba(0, 149, 199);">
+        <div class="col-12 p-3 text-center">
+          <h3 style="text-decoration: underline;">What we offer</h3>
+        </div>
+        <div class="col-md-3 col-sm-9 text-center p-md-5 p-sm-2">
+          <i class="far fa-gem icon-style"></i>
+          <h6 class="p-2" style="color: #1d1d1d;">Best Quality Service</h6>
+          <p class="text-muted px-2">All water piping reports are communicated to your service provider immediately</p>
+        </div>
+        <div class="col-md-3 col-sm-9 text-center p-md-5 p-sm-2">
+          <i class="far fa-clock icon-style"></i>
+          <h6 class="p-2" style="color:  #1d1d1d;">24/7 Support</h6>
+          <p class="text-muted px-2">Our phone support is always available to you fulltime</p>
+        </div>
+        <div class="col-md-3 col-sm-9 text-center p-md-5 p-sm-2">
+          <i class="fas fa-medal icon-style"></i>
+          <h6 class="p-2" style="color:  #1d1d1d;">First class customer service</h6>
+          <p class="text-muted px-2">We use state of the art technology to make sure that your needs are fully attended to.</p>
+        </div>
+        <div class="col-12 text-center">
+          <a href="contact.html" class="btn btn-dark" style="background-color:rgba(0, 149, 199)">Contact us</a>
+        </div>
+      </div>
+       <!--row end-->
+      <a name="reviews"></a>
+      
+      <div class="row shadow py-3 my-3" style="background-color: white;">
+        <div class="col-12 p-3 text-center">
+        <h3 style="text-decoration: underline;">Customer reviews</h3>
+        </div>
+        <div class="col-md-4 col-sm-12 p-md-5 p-sm-2">
+          <div class="card-img-top">
+          <div class="d-flex justify-content-center p-3">
+            <div class="">
+              <img src="Images\eugene.jpg" class="card-img-top circle-img" height="150px" width="150px" alt="">
+            </div>
+          </div>
+         
+          <div class="text-center">
+            <h4 style="color:rgba(0, 149, 199)">Eugine Isalambo</h4>
+            <p class="font-italic" style="color:  #1d1d1d;">"Aqua Track has been a lifesaver for our community. The platform makes it incredibly easy to report any water issues and track the progress of the resolution. The user interface is clean and intuitive, making it accessible even for those who aren't tech-savvy. Since we started using Aqua Track, our response times from the water service provider have improved dramatically. "</p>
+          </div>
+        </div>
+        </div>
+        <div class="col-md-4 col-sm-12 p-md-5 p-sm-2">
+          <div class="card-img-top">
+          <div class="d-flex justify-content-center p-3">
+            <div class="">
+              <img src="Images\adrian.jpg" class="card-img-top circle-img" height="150px" width="150px" alt="">
+            </div>
+          
+          </div>
+          <div class="text-center">
+            <h4 style="color:rgba(0, 149, 199)">Adrian Waweru</h4>
+            <p class="font-italic" style="color:  #1d1d1d;">""Aqua Track has made managing our water services so much easier. The website is very intuitive, allowing me to quickly report any issues and track their resolution. The communication between customers and the water service provider has improved significantly, thanks to this platform. I particularly appreciate the detailed updates on the status of reported problems, which keeps me informed and reassured.The overall experience with Aqua Track has been positive and highly efficient."</p>
+          </div>
+        </div>
+      </div>
+        <div class="col-md-4 col-sm-12 p-md-5 p-sm-2">
+          <div class="card-img-top">
+          <div class="d-flex justify-content-center p-3">
+            <div class="">
+              <img src="Images\irene.jpg" class="card-img-top circle-img" height="150px" width="150px" alt="">
+            </div>
+          </div>
+          
+          <div class="text-center">
+            <h4 style="color:rgba(0, 149, 199)">Irene Waweru</h4>
+            <p class="font-italic" style="color:  #1d1d1d;">"Excellent service! Aqua Track has bridged the communication gap between customers and the water service provider perfectly. Reporting issues is simple, and I love how I can monitor the status of my reports without having to call customer service repeatedly. The platform is efficient and user-friendly, and the support team is always quick to respond to any queries. This has made a significant difference in how we handle water-related issues in our area. Highly recommend Aqua Track to anyone looking for a reliable and efficient communication tool!"</p>
+          </div>
+        </div>
+        </div>
+      </div>
+        </div>
+     
+       <!--row e
+      
+      </body>
+
+    
+
+    <!- Report-form code starts here -->
+    <div class="report-form" id="customerservice" style="background-image: url('https://images.pexels.com/photos/6387866/pexels-photo-6387866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'); background-size: cover; background-position: fill;">
+        <h1> Report Form </h1>
+        <h2> Got an issue? write to us and we will reach you as soon as possible. </h2>
+        <br>
+        <form action="report.php" method="POST">
+            <label for="fname">First Name</label>
+            <input type="text" name="firstname" class="rpt" placeholder="Your name...">
+
+            <label for="lname">Last Name</label>
+            <input type="text" name="lastname" class="rpt" placeholder="Your last name...">
+
+            <label for="email">Email</label>
+            <input type="text" name="email" class="rpt" placeholder="Your email...">
+
+            <label for="phonenumber">Phone number</label>
+            <br><br>
+            <input type="tel" name="phone" class="rpt" placeholder="Your phone number...">
+            <br><br>
+
+            <label for="issue">What is your issue?</label>
+            <select id="issue" name="issue" class="rpt">
+
+                <option value="water connection">New water connection</option>
+                <option value="water shortage">Water shortage </option>
+                <option value="leakage">Water leakage</option>
+                <option value="faulty meter">Faulty meter</option>
+                <option value="other">Any other</option>
+
+            </select>
+
+            <label for="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"
+                class="rpt"></textarea>
+
+            <button type="submit">submit</button>
+
+
+        </form>
+    </div>
+    <!-- Quick-links code ends here -->
+
+    <!-- newsletter code starts here -->
+    <div class="newsletter" style="background-image: url('Images/newsletter.jpg');">
+        <div class="subscribe">
+          
+            <h2 class="subscribe__title" style="color:rgba(0, 149, 199)">Let's keep in touch</h2>
+            <p  class="subscribe__copy" style="color:rgba(255, 255, 255,0.7)">Subscribe to keep up with all the news and updates.<br> Your comfort is always
+                our priority.</p>
+            <form action="subscription.php" method="POST" style="color:rgba(255, 255, 255,0.7)">
+                <div class="form">
+                    <input type="email" class="form__email" placeholder="Enter your email address" name="email"
+                        required />
+                    <button class="form__button">Send</button>
+                </div>
+                <div class="notice" >
+                    <input type="checkbox" checked>
+                    <span class="notice__copy">I agree to my email address being stored and used to recieve monthly
+                        newsletters.</span>
+                </div>
+            <a href="viewnewsletter.php" class="btn btn-primary" style="margin-left: 750px;">View Neswletter</a>
+
+            </form>
+
+        </div>
+    </div>
+    <!-- newsletter code ends here -->
+    <img src="https://wallpaperkenya.co.ke/wp-content/uploads/2022/05/minimal-morning-landscape-8k-gx-scaled.jpg" 
+    style="width: 100%; height: 300px; object-fit: cover;">
+    <!-- footer code starts here -->
+    <footer>
+            <div class="container">
+                <div class="row">
+                  <div class="col-lg-4 col-md-12 text-center">
+                    <hr class="pt-5 mt-5">
+                  </div>
+                  <div class="col-lg-4 col-md-12 text-center">
+                    <br>
+                <span>AquaTrack</span>
+                  </div>
+                  <div class="col-lg-4 col-md-12 text-center">
+                    <hr class="pt-5 mt-5">
+                  </div>
+                </div> <!--row end--><br><br>
+            <p>If you have any questions, do not hesitate to ask them.</p>
+            <div class="row justify-content-center">
+                <div class="col-md-9 col-sm-10 px-2 py-3">
+                  <div class="d-flex justify-content-between py-3 my-2">
+                    <a href="https://twitter.com/irinnahrin" class="icon-link"><i class="fab fa-twitter"></i></a>
+                                <a href="https://web.whatsapp.com/" class="icon-link"><i class="fab fa-whatsapp"></i></a>
+                                <a href="https://www.instagram.com/irene.waweru.100/" class="icon-link"><i class="fab fa-instagram"></i></a>
+                                <a href="www.facebook.com" class="icon-link"><i class="fab fa-facebook"></i></a>
+                  </div>
+                </div>
+              </div>
+            <i class="fa fa-map-marker w3-text-red" style="width:30px"></i> Nairobi,Kenya<br><br>
+            <i class="fa fa-phone w3-text-red" style="width:30px"></i> Phone: 0711791575<br>
+            <div class="py-2 p-1 m-2">
+                <i class="fas fa-envelope-open-text fs-30"></i>
+                <a href="mailto:irenewaweru9@gmail.com"><span class="footer-link-item1"> aquatrackafrica@gmail.com</span></a>
+              </div>
+            <p id="current-year"></p> 
+        </footer>
+    <!-- footer code ends here -->
+
+    <!-- javascript link starts here -->
+    <script src="index.js"></script>
+    <!-- javascript link ends here -->
+</body>
+
+</html>
